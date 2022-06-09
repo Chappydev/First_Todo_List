80ba5d869397da452dc95b408bbce376ed36c3ee
Chappydev <danjtchapman@gmail.com>
2022-06-08 18:43:31 -0600

    Create event listener for createButton

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ const textField = document.getElementById("todo-name");
 const createButton = document.getElementById("create-todo");
 const todoList = document.getElementById("todo-ulist");
 
+function getUserText() {
+
+}
+
+createButton.addEventListener('click', () => {
+  const userText = getUserText();
+
+  resetUserText();
+
+  addNewTodo();
+})
