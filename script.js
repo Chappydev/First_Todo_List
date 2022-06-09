2e2fd05f4eda3a0b66fba4d3b38aae55414c9699
Chappydev <danjtchapman@gmail.com>
2022-06-08 20:12:55 -0600

    Remove getUserText function and replace inside createButton event listener

--- script.js
+++ script.js
@@ -2,19 +2,16 @@ const textField = document.getElementById("todo-name");
 const createButton = document.getElementById("create-todo");
 const todoList = document.getElementById("todo-ulist");
 
-function getUserText(textInputElement) {
-  return textInputElement.innerText;
-}
-
 function resetUserText(textInputElement) {
-  textInputElement.innerHTML = "";
+  textInputElement = "";
 }
 
 createButton.addEventListener('click', () => {
-  const userText = getUserText(textField);
+  const userText = document.getElementById("todo-name").value;
+  console.log(userText);
   // add check for blank text box and accompanying error
 
-  resetUserText();
+  resetUserText(userText);
 
   addNewTodo(userText);
 })
