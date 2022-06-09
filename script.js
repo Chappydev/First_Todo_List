0b9ddd1c385e8f23c1898f0331e563946511e8dd
Chappydev <danjtchapman@gmail.com>
2022-06-08 20:34:09 -0600

    Create working addNewTodo function

--- script.js
+++ script.js
@@ -2,6 +2,25 @@ const textField = document.getElementById("todo-name");
 const createButton = document.getElementById("create-todo");
 const todoList = document.getElementById("todo-ulist");
 
+function addNewTodo(title) {
+  // create new <li> element and <button>
+  const newTodo = document.createElement("li");
+  const newTodoDelete = document.createElement("button");
+  // append appropriate stuff to delete button
+  newTodoDelete.className = "delete-todo";
+  newTodoDelete.type = "button";
+  newTodoDelete.innerText = "X";
+  // assign class to <li>
+  newTodo.className = "todo-item";
+  // append user-created title to the <li> element
+  newTodo.innerText = title;
+  // append delete button to the <li> element
+  newTodo.appendChild(newTodoDelete);
+  // locate the new <li> at the end of the todo list
+  todoList.appendChild(newTodo);
+
+}
+
 function resetUserText(textInputElement) {
   textInputElement = "";
 }
@@ -11,7 +30,7 @@ createButton.addEventListener('click', () => {
   console.log(userText);
   // add check for blank text box and accompanying error
 
-  resetUserText(userText);
-
   addNewTodo(userText);
+
+  resetUserText(userText);
 })
